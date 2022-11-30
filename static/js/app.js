bf94970e1f2545f29e8d823c76223af7fb39085f
Bipul Raman <8312486+BipulRaman@users.noreply.github.com>
2022-12-01 01:02:26 +0530

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,9 @@
-var links = document.getElementsByTagName('nav')[0].getElementsByTagName('a');
-for (var i = 0; i < links.length; i++) {
-    if (document.URL.includes(links[i].href)) {
-        links[i].style.background = '#e4e4e4';
-        break;
+window.onload = function () {
+    var links = document.getElementsByTagName('nav')[0].getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+        if (document.URL.includes(links[i].href)) {
+            links[i].style.background = '#e4e4e4';
+            break;
+        }
     }
-}
+};
