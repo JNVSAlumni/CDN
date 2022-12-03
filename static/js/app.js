ca487be19ed467f66d346cd10a23d64e02086330
Bipul Raman <8312486+BipulRaman@users.noreply.github.com>
2022-12-03 16:27:49 +0530

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,11 @@
-const defaultImageUrl = 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgMpttITxDuksIpdAgTj-NMgEZS2ya5C7HB9iMjz1G8dkbdWE074s_1l2RG6qthkmAeXxFBO-Y6vRE6Copah3D8YXeeDlx5Vq-MWHPHd6bH88uQmDZW0i216HXCElZguKpbLoecYs8yg3dowlt4f_8fndUVGae8hiZlSaNSrXDdkn3ZR6ieMs3WxJ5e/s400-c/IMG_20190721_065427.jpg';
+const DIV_ID_BLOG_POST = 'posts';
+const DIV_ID_LOADER = 'loader';
+const DIV_ID_LEFT_NAV = 'leftNav';
+const DIV_ID_CORNER_NAV = 'cornerNav';
+const CLASS_DISPLAY_NONE = 'hide';
+const BLOG_POSTS_URL = '/feeds/posts/summary?alt=json&amp;max-results=99999';
+const DEFAULT_IMAGE_URL = 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgMpttITxDuksIpdAgTj-NMgEZS2ya5C7HB9iMjz1G8dkbdWE074s_1l2RG6qthkmAeXxFBO-Y6vRE6Copah3D8YXeeDlx5Vq-MWHPHd6bH88uQmDZW0i216HXCElZguKpbLoecYs8yg3dowlt4f_8fndUVGae8hiZlSaNSrXDdkn3ZR6ieMs3WxJ5e/s400-c/IMG_20190721_065427.jpg';
+
 
 window.onload = function () {
     // Highlight navigation
@@ -6,23 +13,23 @@ window.onload = function () {
 
     // check if current url has index.html
     if (document.URL.includes('index.html')) {
-        getBlogPostsToDivId('posts');
+        getBlogPostsToDivId(DIV_ID_BLOG_POST);
     }
 };
 
 // Function Definitions =========================================
 
 function enableLoader() {
-    document.getElementById("loader").classList.remove("hide");
+    document.getElementById(DIV_ID_LOADER).classList.remove(CLASS_DISPLAY_NONE);
 }
 
 function disableLoader() {
-    document.getElementById("loader").classList.add("hide");
+    document.getElementById(DIV_ID_LOADER).classList.add(CLASS_DISPLAY_NONE);
 }
 
 function highlightNavigation() {
-    var leftNavLinks = document.getElementById("leftNav").getElementsByTagName('a');
-    var cornerNavLinks = document.getElementById("cornerNav").getElementsByTagName('a');
+    var leftNavLinks = document.getElementById(DIV_ID_LEFT_NAV).getElementsByTagName('a');
+    var cornerNavLinks = document.getElementById(DIV_ID_CORNER_NAV).getElementsByTagName('a');
     var links = Array.from(leftNavLinks).concat(Array.from(cornerNavLinks));
     links.forEach(link => {
         if (document.URL.includes(link.href)) {
@@ -40,7 +47,7 @@ function getReadableDate(isoDateString) {
 function getBlogPostsToDivId(divId) {
     enableLoader();
     var allPosts = [];
-    fetch('/feeds/posts/summary?alt=json&amp;max-results=99999', {
+    fetch(BLOG_POSTS_URL, {
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
@@ -60,7 +67,7 @@ function getBlogPostsToDivId(divId) {
                     image: post.media$thumbnail?.url.replace('/s72-c/', '/s400-c/')
                 }
                 if (eachPost.image == undefined) {
-                    eachPost.image = defaultImageUrl;
+                    eachPost.image = DEFAULT_IMAGE_URL;
                 }
                 allPosts.push(eachPost);
                 allPosts.push(eachPost);
