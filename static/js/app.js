12ac900122e01e89df9fef62750a2446c43c67f6
Bipul Raman <8312486+BipulRaman@users.noreply.github.com>
2022-12-09 14:18:32 +0530

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -120,6 +120,14 @@ function buildHTMLForAccountLogs(data) {
 var MainApp = angular.module('MainApp', ['MainAppControllers']);
 var MainAppControllers = angular.module('MainAppControllers', []);
 
+function searchsubmit() {
+    $('#alsearch').blur();
+    window.location = window.location.href.split('?')[0] + "?q=" + $('#alsearch').val();
+    return 0;
+}
+
+// ALUMNI SEARCH ------------------
+
 var alumniDSvc = "https://jnvsitamarhi.org/JsonData/alumni.json";
 
 
@@ -143,3 +151,81 @@ MainAppControllers.controller('SearchCtrl', function ($scope, $http) {
          });
     }
 });
+
+
+// ALUMNI FORM ------------------
+
+function profileSelection() {
+    $('#alumni_form_msg').hide();
+    $('#frm_org').hide();
+    $('#frm_dsg').hide();
+    $('#frm_ins').hide();
+    $('#frm_deg').hide();
+    $("#alumni_form_msg").click(function () {
+        $('#alumni_form').show();
+        $('#alumni_form_msg').hide();
+    });
+    $("#entry_1072180231").change(function () {
+        var x = $('#entry_1072180231').val();
+        if (x == 'Student') {
+            $('#entry_1601452556').val("");
+            $('#entry_1485639544').val("");
+            $('#entry_1096792936').val("NA");
+            $('#entry_645748317').val("NA");
+            $('#frm_ins').show();
+            $('#frm_deg').show();
+            $('#frm_org').hide();
+            $('#frm_dsg').hide();
+        }
+        else {
+            $('#entry_1096792936').val("");
+            $('#entry_645748317').val("");
+            $('#entry_1601452556').val("NA");
+            $('#entry_1485639544').val("NA");
+            $('#frm_org').show();
+            $('#frm_dsg').show();
+            $('#frm_ins').hide();
+            $('#frm_deg').hide();
+        }
+    });
+}
+
+function capitalize(textboxid, str) {
+    var output =  str.replace(/([^\W_]+[^\s.-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    document.getElementById(textboxid).value = output;
+}
+
+function validateForm() {
+    var _name = $('#entry_2059521736').val();
+    var _batch = $('#entry_23321339').val();
+    var _exams = $('#entry_2020491004').val();
+    var _mobile = $('#entry_219439507').val();
+    var _email = $('#entry_2110054454').val();
+    var _location = $('#entry_2022448842').val();
+    var _profile = $('#entry_1072180231').val();
+    var _organisation = $('#entry_1096792936').val();
+    var _designation = $('#entry_645748317').val();
+    var _institute = $('#entry_1601452556').val();
+    var _degree = $('#entry_1485639544').val();
+    var _social = $('#entry_1146134730').val();
+
+    if (_name == undefined || _name == "" || _batch == undefined || _batch == "" || _exams == undefined || _exams == "" || _mobile == undefined || _mobile == "" || _email == undefined || _email == "" || _location == undefined || _location == "" || _profile == undefined || _profile == "" || _organisation == undefined || _organisation == "" || _designation == undefined || _designation == "" || _institute == undefined || _institute == "" || _degree == undefined || _degree == "" || _social == "") {
+        alert('Somethimg went wrong !! Refresh the page.');
+        document.getElementById('ss-form').reset();
+        location.reload();
+        return false;
+    }
+    else {
+        submitted = true;
+    }
+}
+
+function alumniFormSuccess() {
+    document.getElementById('ss-form').reset();
+    $('#alumni_form').hide();
+    $('#alumni_form_msg').show();
+}
+
+function alumniFormFailure() {
+    alert('Somethimg went wrong !! Refresh the page.');
+}
