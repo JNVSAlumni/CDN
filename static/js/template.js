bcd52c841d0f88bfed5b050f30d18355be5d8ed5
Bipul Raman <8312486+BipulRaman@users.noreply.github.com>
2022-12-10 09:45:29 +0530

    code updated

---
+++ static/js/template.js
@@ -0,0 +1,28 @@
+
+window.onload = function () {
+    // Highlight navigation
+    highlightNavigation();
+
+    if (document.URL.includes('index.html') || document.URL.includes('/search')) {
+        updateLinkShowAllPosts();
+    }
+};
+
+function updateLinkShowAllPosts() {
+    Array.from(document.getElementsByTagName('a')).forEach((a) => {
+        if (a.innerText == 'Show all posts') {
+            a.href = window.location.origin + '/index.html';
+        }
+    });
+}
+
+function highlightNavigation() {
+    var leftNavLinks = document.getElementById(DIV_ID_LEFT_NAV).getElementsByTagName('a');
+    var cornerNavLinks = document.getElementById(DIV_ID_CORNER_NAV).getElementsByTagName('a');
+    var links = Array.from(leftNavLinks).concat(Array.from(cornerNavLinks));
+    links.forEach(link => {
+        if (document.URL.includes(link.href)) {
+            link.style.background = '#e0e0e0';
+        }
+    });
+}
